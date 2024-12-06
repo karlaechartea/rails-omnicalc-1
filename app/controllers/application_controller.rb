78a87d3d2910c87d1f01e858426d6b2f70f43164
karlaechartea <k.echartea@chicagobooth.edu>
2024-12-06 05:02:17 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,51 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+  def add
+    # Render the form for addition
+  end
+
+  def wizard_add
+    # Perform addition
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num + @second_num
+  end
+
+  def subtract
+    # Render the form for subtraction
+  end
+
+  def wizard_subtract
+    # Perform subtraction
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num - @second_num
+  end
+
+  def multiply
+    # Render the form for multiplication
+  end
+
+  def wizard_multiply
+    # Perform multiplication
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num * @second_num
+  end
+
+  def divide
+    # Render the form for division
+  end
+
+  def wizard_divide
+    # Perform division
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    if @second_num.zero?
+      @result = "Error: Division by zero is not allowed."
+    else
+      @result = @first_num / @second_num
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+      root "calculator#add" # Default to the add form
+    get "/add", to: "calculator#add"
+    get "/wizard_add", to: "calculator#wizard_add"
+    get "/subtract", to: "calculator#subtract"
+    get "/wizard_subtract", to: "calculator#wizard_subtract"
+    get "/multiply", to: "calculator#multiply"
+    get "/wizard_multiply", to: "calculator#wizard_multiply"
+    get "/divide", to: "calculator#divide"
+    get "/wizard_divide", to: "calculator#wizard_divide"
 end
